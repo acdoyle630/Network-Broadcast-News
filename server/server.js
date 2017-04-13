7dfb0f80150b19eebdb452c90cd8cddb959b26ba
Adam <acdoyle630@gmail.com>
2017-04-12 19:53:56 -1000

    MVP

--- server/server.js
+++ server/server.js
@@ -4,13 +4,12 @@ const net = require('net');
 let clients =[];
 
 const server = net.createServer((c) => {
+  clients.push(c);
   c.on('data', ( data ) => {
-    clients.push(c);
-    if(clients.length > 1){
     for(var i = 0; i < clients.length; i++){
-      console.log(clients.length);
+      console.log(c);
+      console.log(clients);
       clients[i].write(data);
-    }
     }
   });
 });
