7104659099fa3c0f47a4e1a3aa08fdd13444fa68
Adam <acdoyle630@gmail.com>
2017-04-13 10:03:43 -1000

    Users can now pick username

--- server/server.js
+++ server/server.js
@@ -2,19 +2,47 @@
 
 const net = require('net');
 let clients =[];
+let users = 1;
 
 const server = net.createServer((c) => {
   clients.push(c);
   c.on('data', ( data ) => {
-    for(var i = 0; i < clients.length; i++){
-      console.log(c);
-      console.log(clients);
-      clients[i].write(data);
+  console.log(c.userName);
+  if(c.userName === undefined){
+    if(checkForUN(data) !== true){
+    c.write('Please select user name by typing UN: followed by your username');
+   } else {
+    c.userName = assignUN(data);
+   }
+  }
+    if(c.userName !== undefined){
+      for(var i = 0; i < clients.length; i++){
+        if(c.userName !== clients[i].userName){
+        clients[i].write(c.userName + ': ' + data);
+       } else {
+        clients[i].write('Me: ' + data );
+       }
+      }
     }
   });
 });
 server.listen(6969, () => {
-  console.log('server');
 });
+
+function checkForUN( data ){
+  let user = data.toString().split('');
+    if( user[0] === 'U' && user[1]=== 'N' && user[2] === ':'){
+      return true;
+  }
+}
+
+function assignUN( data ){
+  let user = data.toString().split('');
+  user.pop();
+  user.shift();
+  user.shift();
+  user.shift();
+  return user.join('');
+}
 
 
